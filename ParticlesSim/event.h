b97c510783e1c5a4682c23474a2fac2f2c7da0c0
Wojciech Matejuk <74838859+WojciechMat@users.noreply.github.com>
2024-05-17 14:33:58 +0200

    GUI (#1)
    - removed unnecessary files
    - created README
    - implemented initial version of GUI
    - made more tests

---
+++ ParticlesSim/event.h
@@ -0,0 +1,26 @@
+#pragma once
+#include "particle.h"
+
+class Event {
+public:
+    double time;
+    Particle* p1;
+    Particle* p2;
+    int countA, countB;
+
+    Event(double t, Particle* a, Particle* b) : time(t), p1(a), p2(b) {
+        countA = a != nullptr ? a->count : -1;
+        countB = b != nullptr ? b->count : -1;
+    }
+
+    bool operator<(const Event& that) const {
+        return this->time > that.time;
+    }
+
+    bool isValid() {
+        if (p1 != nullptr && p1->count != countA) return false;
+        if (p2 != nullptr && p2->count != countB) return false;
+        return true;
+    }
+    friend std::ostream& operator<<(std::ostream& os, const Event& e);
+};
--- ParticlesSim/particle.h
+++ ParticlesSim/particle.h
@@ -1,38 +1,28 @@
 #pragma once
-#include<iostream>
+#include <SFML/System.hpp>
+
 class Particle {
-private:
-	double vx, vy;
-	double x, y;
-	
-	static int particle_count;
-	const bool exists;
-	const double radius;
-	const double mass;
-	int count; 
 public:
+    double vx, vy;
+    double x, y;
 	int id;
-	friend class Event;
-	Particle() : 
-		exists(false), radius(0), mass(0), id(0), count(0), vx(0), vy(0), x(0), y(0){};
-	Particle(double vx, double vy, double x, double y, double mass, double radius) :
-		vx(vx), vy(vy), x(x), y(y), mass(mass), radius(radius), count(0), exists(true), id{++particle_count} {};
-	void move(double t);
-	void draw();
+    static int particle_count;
+    const bool exists;
+    const double mass;
+    int count;
+    const float radius;
 
-	double timeToHit(Particle& p);
-	double timeToHitVerticalWall();
-	double timeToHitHorizontalWall();
+    Particle();
+    Particle(double vx, double vy, double x, double y, double mass, double radius);
+    void move(double t);
+    void draw();
+    double timeToHit(Particle& p);
+    double timeToHitVerticalWall();
+    double timeToHitHorizontalWall();
+    void bounceOffParticle(Particle& p);
+    void bounceOffVerticalWall();
+    void bounceOffHorizontalWall();
+    const bool exist() const { return exists; }
 
-	void bounceOffParticle(Particle& p);
-	void bounceOffVerticalWall();
-	void bounceOffHorizontalWall();
-
-
-	std::pair<double, double> const getPosition() { return std::make_pair(x, y); }
-	std::pair<double, double> const getVelocity() { return std::make_pair(vx, vy); }
-	double const getMass() { return mass; }
-	double const getRadius() { return radius; }
-	const bool exist() { return exists; }
-	
+	friend std::ostream& operator<<(std::ostream& os, const Particle& p);
 };
